27e6714f6c6f1fc974121816d1c818f919e43f66
123mohammed12 <144066927+123mohammed12@users.noreply.github.com>
2023-09-05 17:04:08 +0300

    Add files via upload

---
+++ emcom/test.php
@@ -0,0 +1,10 @@
+<?php 
+$to="ahlasalhmzi@gmail.com";
+$title="hi";
+$body="am mohammed";
+$header="from mohammed@gmail.com ". "\n". "to moha123@gmail.com";
+
+mail($to,$title,$body,$header);
+
+
+?>
